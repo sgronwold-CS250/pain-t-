a448bab3461e9709864b413ed1021271be5b9cca
sgronwold <89616328+sgronwold@users.noreply.github.com>
2024-08-27 15:51:06 -0500

    added editable canvas

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,52 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     Scene scene;
     GridPane grid;
+    Canvas canvas;
+
+    Button menuButtons[] = new Button[1];
 
     public static void main(String[] args) {
-        launch("");
+        System.out.println("Launching Pain(t)");
+        launch(args);
     }
 
     @Override
     public void start(Stage stage) {
+        // basic window things
         stage.setTitle("Paint");
-        stage.setWidth(640);
-        stage.setHeight(480);
+        stage.setWidth(1280);
+        stage.setHeight(720);
 
+        // we setup the grid as we go
         grid = new GridPane();
 
-        scene = new Scene(grid, 640, 480);
-        stage.setScene(scene);        
+        // this scene is the default scene, will include the menu and canvas among other things
+        scene = new Scene(grid, 300, 400);
+        stage.setScene(scene);
+
+        // initialize buttons
+        menuButtons[0] = new Button("Load image...");
+        menuButtons[0].setOnAction(new MenuActionListener());
+        
+        canvas = new Canvas(500, 500);
+        grid.add(canvas, 0, 2);
+
+        for(int i = 0; i < menuButtons.length; i++) {
+            grid.add(menuButtons[i], i, 1);
+        }
+
+        // title
+        Label title = new Label("Let Pain (t) be a strictly increasing function. The value of Pain at any given value t exceeds expectations.");
+        grid.add(title, 0, 0);
 
         stage.show();
     }
---
+++ src/MenuActionListener.java
@@ -0,0 +1,50 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+
+public class MenuActionListener implements EventHandler<ActionEvent> {
+
+    @Override
+    public void handle(ActionEvent e) {
+        System.out.println("Button clicked");
+        Button button;
+        if (e.getSource() instanceof Button) {
+            button = (Button) e.getSource();
+        } else {
+            // otherwise there's nothing for us to do
+            return;
+        }
+
+        // right now we only have one button
+        // TODO make a way to differentiate between the buttons... maybe their text?
+        Scene scene = button.getScene();
+
+        GridPane grid = (GridPane) scene.getRoot();
+
+        Canvas canvas = null;
+        for(Node child: grid.getChildren()) {
+            if(child instanceof Canvas) {
+                canvas = (Canvas) child;
+            }
+        }
+
+        if(canvas == null) {
+            // then we don't have a canvas for some reason?
+            return;
+        }
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        Image img = new Image("./hawks.png");
+        gc.drawImage(img, 0, 0);
+
+    }
+
+}
