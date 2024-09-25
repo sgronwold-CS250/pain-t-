799b56002a8d8bd1d08ddc8a6b7674a3c589737a
Samuel Gronwold <samuelgronwold@gmail.com>
2024-09-25 12:14:15 -0500

    changes to the makefile and readme

--- makefile.sh
+++ makefile.sh
@@ -1,9 +1,10 @@
 clear
 
 cd ./src
-javac --module-path ../lib --add-modules javafx.controls,javafx.swing,javafx.fxml ./*.java -d ../bin/
+javac --module-path ../lib --add-modules javafx.controls,javafx.swing,javafx.fxml -cp ../lib/junit-4.13.2.jar ./*.java -d ../bin/
 cd ..
 
 cd ./bin
+java --module-path ../lib --add-modules javafx.controls,javafx.swing,javafx.fxml -jar ./junit-platform-console-standalone-1.9.3.jar -cp ./ -c UnitTests
 java --module-path ../lib --add-modules javafx.controls,javafx.swing,javafx.fxml Main
 cd ../
