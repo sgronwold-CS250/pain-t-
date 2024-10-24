258759da6945448b4bda0b77a71452fe0c0f0f2d
sgronwold <89616328+sgronwold@users.noreply.github.com>
2024-10-24 14:29:53 -0500

    javadoc'd the CanvasInterface

--- src/CanvasInterface.java
+++ src/CanvasInterface.java
@@ -1,4 +1,14 @@
+/**
+ * An object that interacts with a Canvas object through a listener/callback model.
+ */
 public interface CanvasInterface {
+    /**
+     * Stops the listener in question.
+     */
     public void stopCanvasListener();
+
+    /**
+     * Starts the listener in question.
+     */
     public void startCanvasListener();
 }
