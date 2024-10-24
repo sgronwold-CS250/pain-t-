fc4243305a28c3b6d966ca8929f5d7485a7e4b78
sgronwold <89616328+sgronwold@users.noreply.github.com>
2024-10-24 13:46:45 -0500

    add the javadoc

--- makefile.bat
+++ makefile.bat
@@ -2,7 +2,9 @@ call clear
 
 call cd ./src
 call javac --module-path ../lib --add-modules javafx.controls,javafx.swing,javafx.fxml -cp ../lib/junit-4.13.2.jar ./*.java -d ../bin/
-call cd ..
+call cd ../
+
+call javadoc --module-path ./lib --add-modules javafx.controls,javafx.swing,javafx.fxml -cp ./lib/junit-4.13.2.jar -d ./javadoc ./src/*.java
 
 call cd ./bin
 call java --module-path ../lib --add-modules javafx.controls,javafx.swing,javafx.fxml -jar ./junit-platform-console-standalone-1.9.3.jar -cp ./ -c UnitTests
